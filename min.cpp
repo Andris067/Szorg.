1cfbfcdec2f4763dc4f6fab87cb7aa54aee6c625
Andris067 <andris12345678911@gmail.com>
2025-05-04 23:40:18 +0200

    Add files via upload

---
+++ min.cpp
@@ -0,0 +1,2 @@
+#import<bits/stdc++.h>
+using namespace std;main(){string s;getline(cin,s);cout<<(s==string(s.rbegin(),s.rend()));int n,x[99];cin>>n;for(int i=0;i<n;)cin>>x[i++];sort(x,x+n);for(int i=0;i<n;)cout<<x[i++]<<' ';}
